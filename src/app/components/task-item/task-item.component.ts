0e886fd524a01312dbb9076f72368069677bb214
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2022-05-19 11:18:32 +0300

    Finnish exercise

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -7,10 +10,18 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task:Task = {};
-
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onDelete(task: any){
+    this.onDeleteTask.emit(task);
+  }
 
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +8,25 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+  constructor( private taskService: TaskService ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(res => this.tasks = res);
   }
 
+  deleteTask(task: Task){
+    this.taskService
+      .deleteTask(task)
+      .subscribe(()=> this.tasks = this.tasks.filter(t => t.id != task.id))
+  }
+  
+  editReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addNewTask(newTask: Task){
+    this.taskService.addNewTask(newTask).subscribe(t=>this.tasks.push(t));
+  }
 }
